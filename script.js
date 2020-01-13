6b05de8d1ccef615602c17cf0d58612769f4e97f
person <person@email.com>
2020-01-13 16:34:01 -0600

    weather app

---
+++ script.js
@@ -0,0 +1,28 @@
+
+//api code, not valid yet..
+/*an application to search a city and then get it's resulting weather*/
+function myFunction(){
+    
+    var city = document.getElementById("myText").value;
+
+    $.getJSON("http://api.openweathermap.org/data/2.5/weather?q=" + city + 
+    "&units=metric&APPID=c1529d3a73ef95a02dd287d96a67f644", 
+    function(data){
+        console.log(data);
+
+        var icon = "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
+        var temp = Math.floor(data.main.temp);
+        var weather = data.weather[0].main;
+        var high = Math.floor(data.main.temp_max);
+        var low = Math.floor(data.main.temp_min);
+
+        $('.icon').attr('src', icon);
+        $('.temp').append(temp);//append because your adding into this tag, image was a src itself, thats the difference
+        $('.weather').append(weather);
+        $('.high').append(high);
+        $('.low').append(low);
+        
+    });
+    //not sure how to clear the previous data yet. allow multiple queries without having
+    //to refresh page each time
+}
